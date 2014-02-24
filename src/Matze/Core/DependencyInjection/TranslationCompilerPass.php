080ea555e4f9fb527173c8c8d069b625080b0f99
Matze <matze@mdoetsch.de>
2014-02-24 22:38:16 +0100

    #17 Add translation component

---
+++ src/Matze/Core/DependencyInjection/TranslationCompilerPass.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Matze\Core\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Translation\Dumper\PhpFileDumper;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @CompilerPass
+ */
+class TranslationCompilerPass implements CompilerPassInterface {
+
+	const TAG = 'console';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function process(ContainerBuilder $container) {
+		$translator = $container->getDefinition('Translator');
+
+		$path = ROOT . '/cache/lang/';
+
+		$finder = new Finder();
+		$finder
+			->files()
+			->in(ROOT . '/lang/')
+			->name('*.yaml');
+
+		$yaml_loader = new YamlFileLoader();
+		$dumper = new PhpFileDumper();
+
+		foreach ($finder as $file) {
+			/** @var SplFileInfo $file */
+			$locale = basename($file->getFilename(), '.yaml');
+			$resource = $yaml_loader->load($file->getPathname(), $locale);
+
+			$dumper->dump($resource, [
+				'path' => $path
+			]);
+
+			$translator->addMethodCall('addResource', ['php', sprintf('%smessages.%s.php', $path, $locale), $locale]);
+		}
+
+	}
+}
