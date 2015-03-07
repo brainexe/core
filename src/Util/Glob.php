e0dbcef2888757e1b8bb53d8f220093ebd8fcdfa
matze <matze@mdoetsch.de>
2015-03-07 18:55:44 +0100

    added searchable sensors

---
+++ src/Util/Glob.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BrainExe\Core\Util;
+
+use BrainExe\Annotations\Annotations\Service;
+
+/**
+ * @Service("Glob", public=false)
+ */
+class Glob
+{
+
+    /**
+     * @param string $pattern
+     * @param int $flags
+     * @return array
+     */
+    public function glob($pattern, $flags = null)
+    {
+        return glob($pattern, $flags);
+    }
+}
